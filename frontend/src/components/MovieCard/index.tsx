81a28a56bb810bc442d7c0bd3bdf0b3e8254d61c
Winy Maciel <winymaciel@hotmail.com>
2022-01-16 22:08:32 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -10,16 +10,12 @@ function MovieCard( { movie } : Props) {
 
   return (
     <div>
-      <img
-        className="dsmovie-movie-card-image"
-        src={movie.image}
-        alt={movie.title}
-      />
+      <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
       <div className="dsmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <MovieScore />
+          <h3>{movie.title}</h3>
+          <MovieScore count={movie.count} score={movie.score} />
 
-        <Link to={`/form/${movie.id}`}>
+          <Link to={`/form/${movie.id}`}>
           <div className="btn btn-primary dsmovie-btn">Avaliar</div>
         </Link>
       </div>
